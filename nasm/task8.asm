b7e9822e0d7c1bdde0a227dd32bc86d7679308c6
Maksym Zaika <maksym.zaika@streamlineplan.com>
2024-09-06 11:58:46 +0300

    array print

--- nasm/task8.asm
+++ nasm/task8.asm
@@ -3,55 +3,20 @@ section .data:
     len equ $ - arr
 
 section .bss
-    arraySize resb 1
     sorted resb len
     print_str resb len + 1
 
 section .text
     global _start
 
-; bublesort:
-;     mov rcx, len
-
-;     lea rsi, [arr]
-;     lea rdi, [arr]
-
-;     next:
-;         mov r8, 0
-
-;         mov r9, rsi
-;         mov r10, rdi
-
-;         body:
-;             mov al, [r9]
-;             mov bl, [r10]
-
-;             cmp al, bl
-;             jge next1
-
-;             mov [r9], bl
-;             mov [r10], al
-
-;             mov r8, 1
-
-;         next1:
-;             inc r9
-;             inc r10
-
-;         loop body
-
-;         cmp r8, 0
-;         jne next
-
-;     ret
-
-; Excpects len in rcx, arr in rsi, result in rdi
+; Excpects input arr in rsi
 print_array:
-    mov [arraySize], cl
+    mov rcx, len
+    mov rdi, print_str
 
     print_body:
-        mov eax, [rsi]
-        mov [rdi], eax
+        mov byte al, [rsi]
+        mov byte [rdi], al
         add byte [rdi], '0'
 
         inc rsi
@@ -60,20 +25,16 @@ print_array:
     loop print_body
 
     mov byte [rdi], 0xa
-    sub byte [rdi], arraySize
 
     mov eax, 4
     mov ebx, 1
-        
-    mov ecx, rdi
-    mov edx, [arraySize]
+    mov ecx, print_str
+    mov edx, len + 1
     int 0x80
     ret
-
+    
 _start:
-    mov rcx, len
     lea rsi, [arr]
-    lea rdi, [print_str]
     call print_array
 
 exit:
